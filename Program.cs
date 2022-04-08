31ac240cf5595fef4ca7adaa1b4fc61db1b7c7cc
thomasbrasil46 <thomasbrasilpb@gmail.com>
2022-04-07 23:15:25 -0300

    Saving and openning functions

--- Program.cs
+++ Program.cs
@@ -30,7 +30,19 @@ namespace TextEditor
 
         static void Abrir()
          {
+             Console.Clear();
+             Console.WriteLine("Qual o caminho do arquivo?");
+             string path = Console.ReadLine();
 
+             using (var file = new StreamReader(path))
+             {
+                 string text = file.ReadToEnd();
+                 Console.WriteLine(text);
+             }
+
+             Console.WriteLine("");
+             Console.ReadLine();
+             Menu();
         }
 
         static void Editar()
@@ -62,6 +74,7 @@ namespace TextEditor
             }
 
             Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.ReadLine();
             Menu();
         }
     }
